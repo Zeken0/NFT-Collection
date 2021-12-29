586df0c26df81cc6958d41b6ca3dcc305e33b843
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2021-12-29 14:57:32 +0100

    Setting up the application

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../components/Header.css";
+
+function Header() {
+  return (
+    <div>
+      <h2 className="header">Im the header</h2>
+    </div>
+  );
+}
+
+export default Header;
