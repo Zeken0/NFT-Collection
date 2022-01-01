a6f20467ae4438f25171dd3d885a9e31c336a1cd
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-01-01 21:53:51 +0100

    workin on a function that displays a list of my nfts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Maine />
+      <CollectionCard />
     </div>
   );
 }
---
+++ src/components/CollectionCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CollectionCard = () => {
+  return (
+    <div className="collectionCard">
+      <div></div>
+    </div>
+  );
+};
+
+export default CollectionCard;
