2373fcd4335d0c5dda40cd6f12367273a28f7a01
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-01-01 23:02:18 +0100

    created the function for the list of nfts and styled it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import react from "react";
 import "./App.css";
 import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x7870d0e7Cac591dB68f0ebC14C984184c8eFd2aa&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <CollectionCard />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=w600"
+      />
     </div>
   );
 }
--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,9 +1,19 @@
 import React from "react";
+import weth from "../assets/weth.png";
+import "./CollectionCard.css";
 
-const CollectionCard = () => {
+const CollectionCard = ({ id, name, traits, image }) => {
   return (
     <div className="collectionCard">
-      <div></div>
+      <img src={image} alt="" />
+      <div className="details">
+        <div className="name">{name}</div>
+        <div className="id">*#{id}</div>
+        <div className="priceContainer">
+          <img src={weth} className="wethImage" alt="" />
+          <div className="price">{traits[0]?.value}</div>
+        </div>
+      </div>
     </div>
   );
 };
