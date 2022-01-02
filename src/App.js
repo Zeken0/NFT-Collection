e1202bc053f16d63cabdeafb97652a46d8f3d7b9
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-01-02 18:53:17 +0100

    trying to make the punklist fetch data dynamically

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import react from "react";
 import "./App.css";
-import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
+import PunkList from "./components/PunkList";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -13,7 +12,7 @@ function App() {
       const openseaData = await axios.get(
         "https://testnets-api.opensea.io/assets?asset_contract_address=0x7870d0e7Cac591dB68f0ebC14C984184c8eFd2aa&order_direction=asc"
       );
-      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
     };
     return getMyNfts();
   }, []);
@@ -21,12 +20,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <CollectionCard
-        id={0}
-        name={"Bandana Punk"}
-        traits={[{ value: 7 }]}
-        image="https://lh3.googleusercontent.com/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q=w600"
-      />
+      <PunkList punkListData={punkListData} />
     </div>
   );
 }
--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -5,12 +5,16 @@ import "./CollectionCard.css";
 const CollectionCard = ({ id, name, traits, image }) => {
   return (
     <div className="collectionCard">
-      <img src={image} alt="" />
+      <img src={image} alt="image of the nft" />
       <div className="details">
         <div className="name">{name}</div>
         <div className="id">*#{id}</div>
         <div className="priceContainer">
-          <img src={weth} className="wethImage" alt="" />
+          <img
+            src={weth}
+            className="wethImage"
+            alt="image of the ethereum blockchain"
+          />
           <div className="price">{traits[0]?.value}</div>
         </div>
       </div>
