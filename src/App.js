f3c0b398a37e3dd8f53fb0d716408ff51e051da6
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2022-03-20 23:09:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
-  const [selectedPunk, setSelectedPunk] = useState([3]);
+  const [selectedPunk, setSelectedPunk] = useState([1]);
 
   useEffect(() => {
     const getMyNfts = async () => {
